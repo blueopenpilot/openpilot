24eaa8fea979ed91993bd95d19135fdc73de5433
Pon <bluetulippon@gmail.com>
2024-03-20 03:40:15 +0000

    Update relese files [openpilot]

---
+++ vag_debug.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#
+# Copyright (c) 2020-2024 bluetulippon@gmail.com Chad_Peng(Pon).
+# All Rights Reserved.
+# Confidential and Proprietary - bluetulippon@gmail.com Chad_Peng(Pon).
+#
+
+mkdir -p /data/media/0/build
+mkdir -p /data/media/0/log
---
+++ vag_delete_gitignore.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+#
+# Copyright (c) 2020-2024 bluetulippon@gmail.com Chad_Peng(Pon).
+# All Rights Reserved.
+# Confidential and Proprietary - bluetulippon@gmail.com Chad_Peng(Pon).
+#
+
+rm -rf ./cereal/messaging/.gitignore
+rm -rf ./cereal/visionipc/.gitignore
+rm -rf ./cereal/.gitignore
+rm -rf ./system/ubloxd/.gitignore
+rm -rf ./system/logcatd/.gitignore
+rm -rf ./system/sensord/.gitignore
+rm -rf ./system/loggerd/.gitignore
+rm -rf ./body/.gitignore
+rm -rf ./selfdrive/boardd/.gitignore
+rm -rf ./selfdrive/modeld/.gitignore
+rm -rf ./selfdrive/assets/.gitignore
+rm -rf ./selfdrive/locationd/.gitignore
+rm -rf ./selfdrive/locationd/models/.gitignore
+rm -rf ./selfdrive/ui/soundd/.gitignore
+rm -rf ./selfdrive/ui/.gitignore
+rm -rf ./selfdrive/controls/lib/lateral_mpc_lib/.gitignore
+rm -rf ./selfdrive/controls/lib/longitudinal_mpc_lib/.gitignore
+rm -rf ./selfdrive/navd/.gitignore
+rm -rf ./third_party/acados/.gitignore
+rm -rf ./third_party/.gitignore
+rm -rf ./opendbc/.gitignore
+rm -rf ./panda/board/pedal/.gitignore
+rm -rf ./panda/.gitignore
+rm -rf ./rednose/.gitignore
+rm -rf ./.gitignore
+rm -rf ./common/kalman/.gitignore
+rm -rf ./common/.gitignore
